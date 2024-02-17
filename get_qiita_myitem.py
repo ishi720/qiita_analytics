247a382f3e9a0c78b65c0d311bfdbe2573d096d7
ishi720 <ishizuka720@gmail.com>
2024-02-17 20:48:28 +0900

    initial commit!

---
+++ get_qiita_myitem.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+url = "https://qiita.com/api/v2/items"
+bearer_token = "{Your access token}"
+user = "{Your user name}"
+
+params = {
+    "query": "user:"+ user,
+    "per_page": "50",
+    "page": "1"
+}
+headers = {
+    "Authorization": "Bearer {}".format(bearer_token)
+}
+
+r = requests.get(url, headers=headers, params=params)
+items = r.json()
+
+result = []
+
+for item in items:
+    page_data = {
+        'title': item['title'],
+        'url': item['url'],
+        'likes_count': item['likes_count'],
+        'stocks_count': item['stocks_count'],
+        'page_views_count': item['page_views_count'],
+        'updated_at': item['updated_at']
+    }
+    result.append(page_data)
+
+print(json.dumps(result, indent=2, ensure_ascii=False))

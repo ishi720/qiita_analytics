a09d011de322b25a5a4980fc47e55e2c83918e4e
ishi720 <ishizuka720@gmail.com>
2024-02-21 12:22:16 +0900

    flask導入

--- get_qiita_myitem.py
+++ get_qiita_myitem.py
@@ -1,33 +1,34 @@
 import requests
 import json
 
-url = "https://qiita.com/api/v2/items"
-bearer_token = "{Your access token}"
-user = "{Your user name}"
+def get_qiita_myitem():
+    url = "https://qiita.com/api/v2/items"
+    bearer_token = "{Your access token}"
+    user = "{Your user name}"
 
-params = {
-    "query": "user:"+ user,
-    "per_page": "50",
-    "page": "1"
-}
-headers = {
-    "Authorization": "Bearer {}".format(bearer_token)
-}
-
-r = requests.get(url, headers=headers, params=params)
-items = r.json()
-
-result = []
-
-for item in items:
-    page_data = {
-        'title': item['title'],
-        'url': item['url'],
-        'likes_count': item['likes_count'],
-        'stocks_count': item['stocks_count'],
-        'page_views_count': item['page_views_count'],
-        'updated_at': item['updated_at']
+    params = {
+        "query": "user:"+ user,
+        "per_page": 50,
+        "page": 1
     }
-    result.append(page_data)
+    headers = {
+        "Authorization": "Bearer {}".format(bearer_token)
+    }
 
-print(json.dumps(result, indent=2, ensure_ascii=False))
+    r = requests.get(url, headers=headers, params=params)
+    items = r.json()
+
+    result = []
+
+    for item in items:
+        page_data = {
+            'title': item['title'],
+            'url': item['url'],
+            'likes_count': item['likes_count'],
+            'stocks_count': item['stocks_count'],
+            'page_views_count': item['page_views_count'],
+            'updated_at': item['updated_at']
+        }
+        result.append(page_data)
+
+    return json.dumps(result, indent=2, ensure_ascii=False)
---
+++ main.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+from flask import Flask, render_template
+from get_qiita_myitem import get_qiita_myitem
+import json
+
+app = Flask(__name__)
+
+qiita_data_str = get_qiita_myitem()
+qiita_data = json.loads(qiita_data_str)
+
+@app.route('/')
+def index():
+    # return render_template('index.html')
+    data = 'views.pyのinsert_something部分です。'
+    return render_template('index.html', items=qiita_data)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()

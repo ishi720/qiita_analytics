4753a5062c7bb60857bb3375f783dbff8d954ea2
ishi720 <ishizuka720@gmail.com>
2024-02-25 21:33:35 +0900

    #1 検索結果を維持するように修正

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ def index():
     qiita_data_str = get_qiita_myitem(app.config['QIITA_BEARER_TOKEN'], user_name)
     qiita_data = json.loads(qiita_data_str)
 
-    return render_template('index.html', items=qiita_data)
-
+    return render_template('index.html', items=qiita_data, user_name=user_name)
 
 if __name__ == '__main__':
     app.debug = True
